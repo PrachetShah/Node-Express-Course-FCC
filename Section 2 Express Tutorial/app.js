cdbfb7679b673215e556ff632c6e2f1a0540071e
PrachetShah <prachetshah25@gmail.com>
2022-04-11 11:37:45 +0530

    http basics

--- Section 2 Express Tutorial/app.js
+++ Section 2 Express Tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // Response can be sent via both res.end and res.write
+  // but it is better practise to sent it through res.write and then call it using res.end
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write(`<h1>Home Page</h1>`);
+  res.end();
+});
+
+server.listen(5000);
