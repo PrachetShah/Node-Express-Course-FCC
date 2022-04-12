a947e15350ec4fe7c1ba2b4e00f392983ab5bd82
PrachetShah <prachetshah25@gmail.com>
2022-04-12 19:02:32 +0530

    start of first express app

--- Section 2 Express Tutorial/app.js
+++ Section 2 Express Tutorial/app.js
@@ -1,28 +1,18 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("User hit the resource");
-  res.status(200).send("Home Page");
-});
+// Any folder can be added but general practise is to create a separate public folder
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>Invalid Request</h1>");
+  res.status(404).send("Resource not Found");
 });
 
 app.listen(5000, () => {
   console.log("Server listening on Port 5000");
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
