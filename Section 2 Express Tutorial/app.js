77d91526172be811bc84a5b2b6407356ac7bdf73
PrachetShah <prachetshah25@gmail.com>
2022-04-12 19:54:54 +0530

    custom urls, params in express node

--- Section 2 Express Tutorial/app.js
+++ Section 2 Express Tutorial/app.js
@@ -1,17 +1,39 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-// Any folder can be added but general practise is to create a separate public folder
-// setup static and middleware
-app.use(express.static("./public"));
+const { products } = require("./data");
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("Resource not Found");
+app.get("/api/products", (req, res) => {
+  // sending only id, name, image. Custom Return
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  //console.log(req.params); //prints {productID: 1} if 1 is passed
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+
+  // if product does not exist
+  if (!singleProduct) {
+    res.status(404).send("Product not Found");
+  }
+
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("Hello World");
 });
 
 app.listen(5000, () => {
