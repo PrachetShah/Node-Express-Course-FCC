70ea60535da3866074509f348e0380a8ec1831ed
PrachetShah <prachetshah25@gmail.com>
2022-04-26 20:35:29 +0530

    added projects starter files

--- Section 2 Express Tutorial/routes/people.js
+++ Section 2 Express Tutorial/routes/people.js
@@ -10,10 +10,16 @@ const {
 } = require("../controllers/people");
 
 // we alreaady have base in app.js so we remove routes
-router.get("/", getPeople);
-router.post("/", createPerson);
-router.post("/postman", createPersonPostman);
-router.put("/:id", updatePerson);
-router.delete("/:id", deletePerson);
+// Method 1
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
+
+// Method 2
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
