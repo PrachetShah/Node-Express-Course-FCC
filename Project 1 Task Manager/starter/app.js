ee8659fc2c0353c974c2775fb89183b884837311
PrachetShah <prachetshah25@gmail.com>
2022-04-27 21:40:55 +0530

    cinfiguration of MongoDB in env

--- Project 1 Task Manager/starter/app.js
+++ Project 1 Task Manager/starter/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 //middleware
 // parse form data
@@ -17,6 +19,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, (req, res) => {
-  console.log(`Server Listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server Listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- Project 1 Task Manager/starter/db/connect.js
+++ Project 1 Task Manager/starter/db/connect.js
@@ -1,2 +1,13 @@
-const connectionStrong =
-  "mongodb+srv://prachet:<password>@nodeexpressprojcts.ayg0j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+
+// we get deprecation warnings in v5 of mongoose, to solve it we use following params
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
