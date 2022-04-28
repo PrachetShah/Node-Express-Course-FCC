c334e014ce57850cfe3a9a7230636e6235fdcd08
PrachetShah <prachetshah25@gmail.com>
2022-04-28 10:06:08 +0530

    added basic validators to schema

--- Project 1 Task Manager/starter/models/task.js
+++ Project 1 Task Manager/starter/models/task.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  // Basic Validators
+  name: {
+    type: String,
+    // we can also add a custom msg
+    required: [true, "must provide a name"],
+    trim: true,
+    maxlength: [20, "name cannot exceed 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
